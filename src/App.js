2b1a5af4f62ca2d4c6ff8896b9d372e31273af56
Pål Hamel <pal.hamel@gmail.com>
2022-01-26 13:35:54 +0100

    add array for buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Wrapper from './components/Wrapper';
+import Wrapper from './components/Wrapper'
+import Screen from './components/Screen'
+import ButtonBox from './components/ButtonBox'
+import Button from './components/Button'
+
+// array for creating buttons:
+const btnValues = [
+  ['C', '+-', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Calc it easy:
-        </p>
-
+    <>
+      <header>
+        <p>Calc it easy:</p>
       </header>
-      <Wrapper />
-    </div>
-  );
+      <Wrapper>
+        <Screen value='10' />
+        <ButtonBox>
+          <Button
+            className=''
+            value='8'
+            onClick={(value) => {
+              console.log('btn clicked', value)
+            }}
+          />
+        </ButtonBox>
+      </Wrapper>
+    </>
+  )
 }
 
-export default App;
+export default App
